d8fa52fc42455433ea9cc7e29341da79e9b227fe
vbeidos <>
2020-03-04 13:40:12 -0500

    Doors now open

---
+++ Ported/MontrealMetro/Assets/Scripts/Systems/DoorPanelAnimationSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class DoorPanelAnimationSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities.ForEach((ref Translation translation, ref DoorPanelComponent doorPanelComponent) =>
+        {
+            switch (doorPanelComponent.DoorState)
+            {
+                case DoorState.Closed:
+                    break;
+                case DoorState.Closing:
+                    {
+                        float displacement = doorPanelComponent.DoorDirection == DoorDirection.Left ? 0.3f : -0.3f;
+                        translation.Value = new float3(translation.Value.x + displacement, 0, translation.Value.z);
+                        doorPanelComponent.DoorState = DoorState.Closed;
+                    }          
+                    break;
+                case DoorState.Open:
+                    break;
+                case DoorState.Opening:
+                    {
+                        float displacement = doorPanelComponent.DoorDirection == DoorDirection.Left ? -0.3f : 0.3f;
+                        translation.Value = new float3(translation.Value.x + displacement, 0, translation.Value.z);
+                        doorPanelComponent.DoorState = DoorState.Open;
+                    }                   
+                    break;
+
+            }
+
+        }).Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
