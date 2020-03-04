1c7312939115afaab057efc70741755ec539ddda
Carle <carle.cote@gmail.com>
2020-03-04 14:24:43 -0500

    Spawning 2 Commuters

---
+++ Ported/MontrealMetro/Assets/Scripts/Authoring/CommuterSpawnPointAuthoring.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Entities;
+using System.Collections.Generic;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class CommuterSpawnPointAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public GameObject CommuterPrefab;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        Entity prefab = conversionSystem.GetPrimaryEntity(CommuterPrefab);
+        dstManager.AddComponentData(entity, new CommuterSpawnPointComponent { CommuterPrefab = prefab });
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        referencedPrefabs.Add(CommuterPrefab);
+    }
+}
--- Ported/MontrealMetro/Assets/Scripts/Systems/CommutersSpawnerSystem.cs
+++ Ported/MontrealMetro/Assets/Scripts/Systems/CommutersSpawnerSystem.cs
@@ -1,10 +1,19 @@
 ﻿using Unity.Entities;
 using Unity.Jobs;
+using Unity.Transforms;
 
+[UpdateInGroup(typeof(InitializationSystemGroup))]
 public class CommutersSpawnerSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
+        Entities.WithStructuralChanges().ForEach((Entity e, ref Translation translation, ref CommuterSpawnPointComponent commuterSpawnerComponent) =>
+        {
+            var commuter = EntityManager.Instantiate(commuterSpawnerComponent.CommuterPrefab);
+            EntityManager.SetComponentData(commuter, new Translation { Value = translation.Value });
+
+            EntityManager.DestroyEntity(e);
+        }).Run();
         return default;
     }
 }
