f6d99336378ad23ee8a458e88ecea1417262532a
Carle <carle.cote@gmail.com>
2020-03-04 13:38:45 -0500

    Empty Commuter and Commuter Spawner

---
+++ Ported/MontrealMetro/Assets/Scripts/Systems/CommutersSpawnerSystem.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class CommutersSpawnerSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return default;
+    }
+}
