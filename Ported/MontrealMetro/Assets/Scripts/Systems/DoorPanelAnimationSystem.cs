7e54c46e90259aa3bb14580f1bb5a02e109ddc01
vbeidos <>
2020-03-04 14:58:32 -0500

    Fix doors opening

--- Ported/MontrealMetro/Assets/Scripts/Systems/DoorPanelAnimationSystem.cs
+++ Ported/MontrealMetro/Assets/Scripts/Systems/DoorPanelAnimationSystem.cs
@@ -3,6 +3,7 @@ using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 public class DoorPanelAnimationSystem : JobComponentSystem
 {
@@ -16,8 +17,8 @@ public class DoorPanelAnimationSystem : JobComponentSystem
                     break;
                 case DoorState.Closing:
                     {
-                        float displacement = doorPanelComponent.DoorDirection == DoorDirection.Left ? 0.3f : -0.3f;
-                        translation.Value = new float3(translation.Value.x + displacement, 0, translation.Value.z);
+                        float displacement = doorPanelComponent.DoorDirection == DoorDirection.Left ? -0.3f : 0.3f;
+                        translation.Value = new float3(translation.Value.x - displacement, translation.Value.y, translation.Value.z);
                         doorPanelComponent.DoorState = DoorState.Closed;
                     }          
                     break;
@@ -26,7 +27,7 @@ public class DoorPanelAnimationSystem : JobComponentSystem
                 case DoorState.Opening:
                     {
                         float displacement = doorPanelComponent.DoorDirection == DoorDirection.Left ? -0.3f : 0.3f;
-                        translation.Value = new float3(translation.Value.x + displacement, 0, translation.Value.z);
+                        translation.Value = new float3(translation.Value.x + displacement, translation.Value.y, translation.Value.z);
                         doorPanelComponent.DoorState = DoorState.Open;
                     }                   
                     break;
