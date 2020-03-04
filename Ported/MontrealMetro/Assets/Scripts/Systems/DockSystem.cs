6406638e22e3947d88c4fbd0cc387442ef5ba1e2
gleplang-eidos <gleplang@eidosmontreal.com>
2020-03-04 10:05:01 -0500

    Add 2 systems

---
+++ Ported/MontrealMetro/Assets/Scripts/Systems/DockSystem.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class DockSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return default;
+    }
+}
