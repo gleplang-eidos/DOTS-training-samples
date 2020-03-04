5e4bc9420360a3bbce5fe765d9658c77d061875d
Carle <carle.cote@gmail.com>
2020-03-04 15:48:00 -0500

    Add queues

---
+++ Ported/MontrealMetro/Assets/Scripts/Components/QueueBufferElementData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct QueueBufferElementData : IBufferElementData
+{
+    public Entity Commuter;
+}
---
+++ Ported/MontrealMetro/Assets/Scripts/Components/QueueComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[Serializable]
+public struct QueueComponent : IComponentData
+{
+    public float PositioningOffset;
+} 
+
+
