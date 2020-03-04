e4f8555c0601c534344ff928273e2e271c3bb0c2
etienne fournier <efournier@eidosmontreal.com>
2020-03-04 13:51:27 -0500

    Added TrainComponent and WagonMonoTag.  Edited corresponding prefabs

---
+++ Ported/MontrealMetro/Assets/Scripts/Authoring/TrainAuthoringComponent.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class TrainAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        // Get all the children
+        var children = gameObject.GetComponentsInChildren<WagonMonoTag>();
+
+        // Create the shared component
+        var train = new TrainComponent { Wagons = new UnsafeList<Entity> (children.Length, Unity.Collections.Allocator.Persistent) };
+
+        // Fill the list of wagon as well as assigning the shared component to every wagon.
+        //foreach (var childTransform in children)
+        //{
+        //    var wagonEntity = conversionSystem.GetPrimaryEntity(childTransform.gameObject);
+        //    train.Wagons.Add(wagonEntity);
+        //    dstManager.AddSharedComponentData(wagonEntity, train);
+        //}
+
+        foreach (var childTransform in children)
+        {
+            train.Wagons.Add(conversionSystem.GetPrimaryEntity(childTransform.gameObject));
+        }
+
+        foreach (var childTransform in children)
+        {
+            dstManager.AddSharedComponentData(conversionSystem.GetPrimaryEntity(childTransform.gameObject), train);
+        }
+    }
+}
---
+++ Ported/MontrealMetro/Assets/Scripts/Components/TrainComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[Serializable]
+public struct TrainComponent : ISharedComponentData
+{
+    public UnsafeList<Entity> Wagons;   
+}
