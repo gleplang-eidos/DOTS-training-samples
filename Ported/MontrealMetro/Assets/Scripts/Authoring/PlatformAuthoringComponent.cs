aae8c66d8606acce2ecdbe69a9e96cfcf182154d
etienne fournier <efournier@eidosmontreal.com>
2020-03-05 09:42:08 -0500

    Split Platform component into authoring and runtime to add the dynamic buffer of Commuter reference

---
+++ Ported/MontrealMetro/Assets/Scripts/Authoring/PlatformAuthoringComponent.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class PlatformAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity
+{
+    [SerializeField]
+    float DockedTime;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new PlatformComponent { DockedTime = DockedTime });
+        dstManager.AddBuffer<EntityBufferElementData>(entity);
+    }
+}
