e63875650aaaba1fdf27f4cb1ca78fa033f14f4c
etienne fournier <efournier@eidosmontreal.com>
2020-03-05 09:29:56 -0500

    Renamed QueueBufferElementData to EntityBufferElementData

---
+++ Ported/MontrealMetro/Assets/Scripts/Components/EntityBufferElementData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct EntityBufferElementData : IBufferElementData
+{
+    public Entity entity;
+}
--- Ported/MontrealMetro/Assets/Scripts/Components/QueueBufferElementData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using Unity.Entities;
-
-[Serializable]
-public struct QueueBufferElementData : IBufferElementData
-{
-    public Entity Commuter;
-}
