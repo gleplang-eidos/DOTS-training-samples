4264f096605f79a0e1b2f464cd1196892bb0a0dd
Carle <carle.cote@gmail.com>
2020-03-05 11:56:36 -0500

    Assign queues to platform

--- Ported/MontrealMetro/Assets/Scripts/Authoring/PlatformAuthoringComponent.cs
+++ Ported/MontrealMetro/Assets/Scripts/Authoring/PlatformAuthoringComponent.cs
@@ -1,17 +1,34 @@
-﻿using Unity.Entities;
+﻿using System.Collections.Generic;
+using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 
 [DisallowMultipleComponent]
 [RequiresEntityConversion]
-public class PlatformAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity
+public class PlatformAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
 {
     [SerializeField]
     float DockedTime;
+    [SerializeField]
+    List<GameObject> Queues = new List<GameObject>();
 
     public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
     {
         dstManager.AddComponentData(entity, new PlatformComponent { DockedTime = DockedTime });
-        dstManager.AddBuffer<QueueBufferElementData>(entity);
+        var buffer = dstManager.AddBuffer<QueueBufferElementData>(entity);
+
+        for (int i = 0; i < Queues.Count; i++)
+        {
+            Entity platform = conversionSystem.GetPrimaryEntity(Queues[i]);
+            buffer.Add(new QueueBufferElementData() { entity = platform });
+        }
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        for (int i = 0; i < Queues.Count; i++)
+        {
+            referencedPrefabs.Add(Queues[i]);
+        }
     }
 }
