0920962e9e49f42a44befc15e596e299210c18e6
vbeidos <>
2020-03-05 15:45:31 -0500

    Added Seats to Wagons

---
+++ Ported/MontrealMetro/Assets/Scripts/Authoring/WagonAuthoringComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class WagonAuthoringComponent : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    [SerializeField]
+    List<GameObject> Seats = new List<GameObject>();
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        FixedList128<Entity> fixedList = new FixedList128<Entity>();
+               
+        for (int i = 0; i < Seats.Count; i++)
+        {
+            Entity seat = conversionSystem.GetPrimaryEntity(Seats[i]);
+            fixedList.Add(seat);
+        }
+
+        dstManager.AddComponentData(entity, new WagonComponent { Seats = fixedList });
+    }
+
+    public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+    {
+        for (int i = 0; i < Seats.Count; i++)
+        {
+            referencedPrefabs.Add(Seats[i]);
+        }
+    }
+}
---
+++ Ported/MontrealMetro/Assets/Scripts/Components/WagonComponent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Entities;
+
+public struct WagonComponent : IComponentData
+{
+    public FixedList128<Entity> Seats;
+}
