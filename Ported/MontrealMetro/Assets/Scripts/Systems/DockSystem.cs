e78306b98e010195eb95aeecd67f1e3a2c6a8867
etienne fournier <efournier@eidosmontreal.com>
2020-03-06 10:21:57 -0500

    Stop train at platform end

--- Ported/MontrealMetro/Assets/Scripts/Systems/DockSystem.cs
+++ Ported/MontrealMetro/Assets/Scripts/Systems/DockSystem.cs
@@ -9,13 +9,13 @@ public class DockSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     { 
-        var lineWaypointComponents = GetComponentDataFromEntity<LineWaypointComponent>(true);
+        var railComponents = GetComponentDataFromEntity<RailComponent>(true);
         var localToWorlds = GetComponentDataFromEntity<LocalToWorld>(true);
         var doorPanels = GetComponentDataFromEntity<DoorPanelComponent>();
 
         Entities.
             WithReadOnly(localToWorlds).
-            WithReadOnly(lineWaypointComponents).
+            WithReadOnly(railComponents).
             WithStructuralChanges().
             WithoutBurst().
             WithNone<DockedTag>().
@@ -23,9 +23,9 @@ public class DockSystem : JobComponentSystem
             {
                 
                 bool destinationIsPlatformStart = false;
-                if (lineWaypointComponents.HasComponent(destination.Target))
+                if (railComponents.HasComponent(destination.Target))
                 {
-                    destinationIsPlatformStart = lineWaypointComponents[destination.Target].Type == RailMarkerType.PLATFORM_START;
+                    destinationIsPlatformStart = railComponents[destination.Target].Type == RailMarkerType.PLATFORM_END;
                 }
 
                 // If close enough to a dock entry, stop every wagon by adding the docked tag.
