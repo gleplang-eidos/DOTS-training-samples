87c9f3ad706bd084d7bf0567ea37b06872cd29fd
Carle <carle.cote@gmail.com>
2020-03-06 10:26:18 -0500

    Add WagonBoardingSystem

--- Ported/MontrealMetro/Assets/Scripts/Systems/CommuterNavigationSystem.cs
+++ Ported/MontrealMetro/Assets/Scripts/Systems/CommuterNavigationSystem.cs
@@ -6,25 +6,29 @@ using Unity.Transforms;
 using Unity.Collections;
 using Unity.Mathematics;
 
+[UpdateAfter(typeof(WagonBoardingSystem))]
 public class CommuterNavigationSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
         var deltaTime = Time.DeltaTime;
-        Entities.ForEach((Entity e, CommuterComponent commuter, ref Translation translation, ref Rotation rotation) =>
+        Entities.WithoutBurst().ForEach((Entity e, ref CommuterComponent commuter, ref Translation translation, ref Rotation rotation) =>
         {
-            float3 direction = new float3(commuter.targetPosition.x, 0, commuter.targetPosition.z)
+            if(!commuter.isAtTargetPosition)
+            {
+                float3 direction = new float3(commuter.targetPosition.x, 0, commuter.targetPosition.z)
             - new float3(translation.Value.x, 0, translation.Value.z);
 
-            // already there
-            if (math.length(direction) < 0.1f)
-            {
-                return;
+                // already there
+                if (math.length(direction) < 0.1f)
+                {
+                    commuter.isAtTargetPosition = true;
+                    return;
+                }
+
+                direction = math.normalize(direction);
+                translation.Value += direction * commuter.speed * deltaTime;
             }
-
-            direction = math.normalize(direction);
-            translation.Value += direction * commuter.speed * deltaTime;
-
         }).Run();
 
         return default;
---
+++ Ported/MontrealMetro/Assets/Scripts/Systems/WagonBoardingSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+[UpdateAfter(typeof(QueueSystem))]
+public class WagonBoardingSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities
+            .WithoutBurst()
+            .ForEach((Entity entity, ref Translation translation, ref CommuterComponent commuterComponent, ref CommuterWagonComponent commuterWagonComponent) =>
+            {
+                if(!commuterComponent.targetPosition.Equals(commuterWagonComponent.EntryPosition) &&
+                    !commuterComponent.targetPosition.Equals(commuterWagonComponent.SeatPosition))
+                {
+                    commuterComponent.targetPosition = commuterWagonComponent.EntryPosition;
+                    commuterComponent.isAtTargetPosition = false;
+                }
+
+                if(commuterComponent.isAtTargetPosition &&
+                   commuterComponent.targetPosition.Equals(commuterWagonComponent.EntryPosition))
+                {
+                    commuterComponent.targetPosition = commuterWagonComponent.SeatPosition;
+                    commuterComponent.isAtTargetPosition = false;
+                }
+            }
+        ).Run();
+
+        return inputDeps;
+    }
+}
